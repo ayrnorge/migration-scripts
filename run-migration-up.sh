98aa9de662c0e4c039e06914204c80c758d2d6c4
Simen A. W. Olsen <cobraz@cobraz.no>
2020-01-07 15:52:07 +0100

    feat: Add test mode

--- run-migration-up.sh
+++ run-migration-up.sh
@@ -8,8 +8,15 @@ echo -n "name of this migration: "
 read userInput
 if [[ -n "$userInput" ]]; then
     LOG_FILE=/media/disk/upload-$userInput.log
-    screen -dmS migration-up rclone --log-file $LOGFILE copy \
-        $FILE_DIRECTORY \
-        $TO_SOURCE \
-        -P
+    if [[ -n "$TEST_MODE" ]]; then
+        rclone --log-file "$LOGFILE" copy \
+            $FILE_DIRECTORY \
+            $TO_SOURCE \
+            -P
+    else
+        screen -dmS migration-up rclone --log-file $LOGFILE copy \
+            $FILE_DIRECTORY \
+            $TO_SOURCE \
+            -P
+    fi
 fi
