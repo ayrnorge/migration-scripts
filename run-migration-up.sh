b700bf72943cf48700e8aff14db1dbd13cadc0aa
Simen A. W. Olsen <cobraz@cobraz.no>
2020-01-07 16:01:53 +0100

    fix: add eval

--- run-migration-up.sh
+++ run-migration-up.sh
@@ -8,13 +8,12 @@ echo -n "name of this migration: "
 read userInput
 if [[ -n "$userInput" ]]; then
     
-    COMMAND="rclone --log-file \"$LOGFILE_PATH/upload-$userInput.log\" -vv copy \
-            $FILE_DIRECTORY \
-            $TO_SOURCE \
-            -P"
+    COMMAND="rclone --log-file \"$LOGFILE_PATH/upload-$userInput.log\" -vv copy $FILE_DIRECTORY $TO_SOURCE -P"
+    echo "[Running Command]: $COMMAND
+    "
     if [[ -n "$TEST_MODE" ]]; then
         eval $COMMAND
     else
-        screen -dmS migration-$userInput $COMMAND
+        eval "screen -dmS migration-$userInput $COMMAND"
     fi
 fi
