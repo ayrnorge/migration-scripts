732e732d24f96e8e1df2b8088ff5c43f4cd3193d
Simen A. W. Olsen <cobraz@cobraz.no>
2020-01-07 15:49:40 +0100

    fix: statement

--- run-migration-up.sh
+++ run-migration-up.sh
@@ -6,7 +6,7 @@ FILE_DIRECTORY=/media/disk/minimeis
 
 echo -n "name of this migration: "
 read userInput
-if [[ -n "$userInput" ]]
+if [[ -n "$userInput" ]]; then
     LOG_FILE=/media/disk/upload-$userInput.log
     screen -dmS migration-up rclone --log-file $LOGFILE copy \
         $FILE_DIRECTORY \
