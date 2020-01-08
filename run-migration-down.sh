91754f79aa7f7cacae197600477d09b3b6018e76
Simen A. W. Olsen <cobraz@cobraz.no>
2020-01-08 13:22:23 +0100

    fix: kebabcase

--- run-migration-down.sh
+++ run-migration-down.sh
@@ -4,35 +4,10 @@ FROM_SOURCE="dropbox:"
 FILE_DIRECTORY=/media/disk/files
 LOGFILE_PATH=/media/disk
 
-detect() {
-  # Return normalized string, all lower and with spaces as separators
-  arg=( "${@:-}" )
-  helper=""
-  for i in '_' '-' ' '; do
-    if [[ ${arg[*]} == *$i* ]]; then
-      helper="yes"
-      echo "${arg[*]}" | tr '[:upper:]' '[:lower:]' | tr -s "${i}" ' '
-    fi
-  done
-  # If '_','-' and ' ' are not used as a separator try by case
-  if [[ -z $helper ]] && [[ ${arg[0]} != '' ]]; then
-    dif_case "${@:-}" | tr '[:upper:]' '[:lower:]'
-  fi
-}
-
-replace() {
-  # Read from stdin and replace ' ' with $1
-  cat | tr -s ' ' "${1}"
-}
-
-kebab() {
-  detect "${@:-}" | replace '-'
-}
-
 echo -n "folder name: "
 read folderName
 if [[ -n "$folderName" ]]; then
-    SAFE_FOLDERNAME=$(kebab $folderName)
+    SAFE_FOLDERNAME=$(echo $folderName | sed -r 's/([a-z0-9])([A-Z])/\1_\L\2/g')
     COMMAND="rclone --log-file \"$LOGFILE_PATH/upload-$SAFE_FOLDERNAME.log\" -vv copy \"$FROM_SOURCE/$folderName\" \"$FILE_DIRECTORY/$folderName\"  -P"
     echo "[Running Command]: $COMMAND
     "
