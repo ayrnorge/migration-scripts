07f6caec2121855c175fa168374e74755c310091
Simen A. W. Olsen <cobraz@cobraz.no>
2020-01-08 13:28:07 +0100

    fix: replace local folderName

--- run-migration-down.sh
+++ run-migration-down.sh
@@ -8,7 +8,7 @@ echo -n "folder name: "
 read folderName
 if [[ -n "$folderName" ]]; then
     SAFE_FOLDERNAME=$(echo $folderName | tr " " _)
-    COMMAND="rclone --log-file \"$LOGFILE_PATH/upload-$SAFE_FOLDERNAME.log\" -vv copy \"$FROM_SOURCE/$folderName\" \"$FILE_DIRECTORY/$folderName\"  -P"
+    COMMAND="rclone --log-file \"$LOGFILE_PATH/upload-$SAFE_FOLDERNAME.log\" -vv copy \"$FROM_SOURCE/$folderName\" \"$FILE_DIRECTORY/$SAFE_FOLDERNAME\"  -P"
     echo "[Running Command]: $COMMAND
     "
     if [[ -n "$TEST_MODE" ]]; then
