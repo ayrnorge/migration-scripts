9a1851ddd55fcb3e79613f4c5a3c055e2461b8fe
Simen A. W. Olsen <cobraz@cobraz.no>
2020-01-26 15:07:26 +0100

    chore: make safe_logfile safer

--- migrate-from-disk-to-disk.sh
+++ migrate-from-disk-to-disk.sh
@@ -10,7 +10,7 @@ fi
 
 
 if [ -n "$FROM_CONFIG" ] || [ -n "$TO_CONFIG" ]; then
-    SAFE_LOGFILENAME=$(echo $FROM_CONFIG-$TO_CONFIG | tr " " _)
+    SAFE_LOGFILENAME=$(echo $FROM_CONFIG-$TO_CONFIG | tr " " _  | tr ":" _  | tr "/" _)
     COMMAND="rclone --log-file \"$LOGFILE_PATH/disk-to-disk-$SAFE_LOGFILENAME.log\" -vv copy \"$FROM_CONFIG\" \"$TO_CONFIG\" -P --drive-skip-gdocs"
     echo "[Running Command]: $COMMAND
     "
