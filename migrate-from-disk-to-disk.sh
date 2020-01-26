f33bee1639bea95191b2122dc3257bb000499d95
Simen A. W. Olsen <cobraz@cobraz.no>
2020-01-26 14:03:26 +0100

    chore: add disk-to-disk

---
+++ migrate-from-disk-to-disk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FROM_CONFIG=$1
+TO_CONFIG=$2
+LOGFILE_PATH=$PWD/logs
+
+if [[ !-d "$LOGFILE_PATH" ]]; then
+    mkdir $LOGFILE_PATH
+fi
+
+
+if [[ -n "$FROM_CONFIG" ] || [ -n "$TO_CONFIG" ]]; then
+    COMMAND="rclone --log-file \"$LOGFILE_PATH/disk-to-disk-$SAFE_FOLDERNAME.log\" -vv copy \"$FROM_CONFIG\" \"$TO_CONFIG\"  -P"
+    echo "[Running Command]: $COMMAND
+    "
+    if [[ -n "$TEST_MODE" ]]; then
+        eval $COMMAND
+    else
+        eval "screen -dmS up-$SAFE_FOLDERNAME $COMMAND"
+    fi
+fi
